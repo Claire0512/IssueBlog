770ebc2bc1b43de6c93ad3ad379fadfffdfe0d35
Claire0512 <claire32246223@gmail.com>
2024-03-27 01:21:18 +0800

    Implement role-based conditional rendering on profile page

--- src/app/(main-interface)/profile/page.tsx
+++ src/app/(main-interface)/profile/page.tsx
@@ -47,38 +47,40 @@ function ProfilePage() {
 						</h2>
 					)}
 				</div>
-				<div className="mt-8 flex w-full items-center justify-center gap-4">
-					<Card className="w-[14%]">
-						<CardContent className="rounded-xl border-l-[16px] border-[#FAC13E] bg-white">
-							<CardHeader className="text-center text-xl text-[#412517]">
-								發布的文章
-							</CardHeader>
-							<p className="text-bold text-center text-2xl text-[#412517]">
-								{githubData.issuesCount} 則
-							</p>
-						</CardContent>
-					</Card>
-					<Card className="w-[14%]">
-						<CardContent className="rounded-xl border-l-[16px] border-[#FAC13E] bg-white">
-							<CardHeader className="text-center text-xl text-[#412517]">
-								文章被留言
-							</CardHeader>
-							<p className="text-bold text-center text-2xl">
-								{githubData.commentsCount} 次
-							</p>
-						</CardContent>
-					</Card>
-					<Card className="w-[14%]">
-						<CardContent className="rounded-xl border-l-[16px] border-[#FAC13E] bg-white">
-							<CardHeader className="text-center text-xl text-[#412517]">
-								獲得的表情
-							</CardHeader>
-							<p className="text-bold text-center text-2xl">
-								{githubData.reactionsCount} 個
-							</p>
-						</CardContent>
-					</Card>
-				</div>
+				{session?.username === process.env.NEXT_PUBLIC_USER_NAME && (
+					<div className="mt-8 flex w-full items-center justify-center gap-4">
+						<Card className="w-[14%]">
+							<CardContent className="rounded-xl border-l-[16px] border-[#FAC13E] bg-white">
+								<CardHeader className="text-center text-xl text-[#412517]">
+									發布的文章
+								</CardHeader>
+								<p className="text-bold text-center text-2xl text-[#412517]">
+									{githubData.issuesCount} 則
+								</p>
+							</CardContent>
+						</Card>
+						<Card className="w-[14%]">
+							<CardContent className="rounded-xl border-l-[16px] border-[#FAC13E] bg-white">
+								<CardHeader className="text-center text-xl text-[#412517]">
+									文章被留言
+								</CardHeader>
+								<p className="text-bold text-center text-2xl">
+									{githubData.commentsCount} 次
+								</p>
+							</CardContent>
+						</Card>
+						<Card className="w-[14%]">
+							<CardContent className="rounded-xl border-l-[16px] border-[#FAC13E] bg-white">
+								<CardHeader className="text-center text-xl text-[#412517]">
+									獲得的表情
+								</CardHeader>
+								<p className="text-bold text-center text-2xl">
+									{githubData.reactionsCount} 個
+								</p>
+							</CardContent>
+						</Card>
+					</div>
+				)}
 			</div>
 		</div>
 	);
